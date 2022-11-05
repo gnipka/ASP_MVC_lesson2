4ef02ce8b33493ca0e9230a717eec44e665edb90
gnipka <gnipka29@gmail.com>
2022-11-05 21:29:41 +0300

    initialization

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ASP_MVC_lesson2
+{
+    //1. Используя знания о пуле потоков, напишите свой микро пул с использованием новых структур данных.
+    //2. Добавьте в ваш пул настройку для максимального количества регистрируемых потоков или кидайте ошибку
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
